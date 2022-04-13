41a350427fda4f312015fdc25b0469abda37b321
Samuel Martins <francisco.martins@modal.com.br>
2022-04-13 09:05:31 -0300

    first commit

---
+++ Sources/SwiftPackage/CustomComponents/ViewCode/ViewCodeComponent.swift
@@ -0,0 +1,68 @@
+//
+//  File.swift
+//  
+//
+//  Created by FRANCISCO SAMUEL DA SILVA MARTINS on 13/04/22.
+//
+
+import UIKit
+
+class ViewCodeComponent : UIView{
+    let titleLabel: UILabel = {
+        let component = UILabel()
+        component.text = "Titulo"
+        component.font = UIFont.boldSystemFont(ofSize: 36)
+        component.adjustsFontSizeToFitWidth = true
+        component.textAlignment = .center
+        component.translatesAutoresizingMaskIntoConstraints = false
+        return component
+    } ()
+    
+    let imageView: UIImageView = {
+        let component = UIImageView()
+        component.image = UIImage(systemName: "pencil")
+        component.contentMode = .scaleAspectFit
+        component.translatesAutoresizingMaskIntoConstraints = false
+        return component
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        buildLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        buildLayout()
+    }
+    
+    func buildLayout(){
+        self.addSubview(titleLabel)
+        self.addSubview(imageView)
+        
+        titleLabel.backgroundColor = .blue
+        imageView.backgroundColor = .cyan
+        
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            titleLabel.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            titleLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 10),
+            titleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            titleLabel.heightAnchor.constraint(lessThanOrEqualToConstant: 40)
+        ])
+        
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            imageView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            imageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+            imageView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -10),
+            imageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+        ])
+    }
+    
+    func configureComponentData(image : UIImage, text : String){
+           titleLabel.text = text
+           imageView.image = image
+    }
+
+}
---
+++ Sources/SwiftPackage/CustomComponents/XIB/XibComponent.swift
@@ -0,0 +1,50 @@
+//
+//  CustomView.swift
+//  XIBMedium
+//
+//  Created by FRANCISCO SAMUEL DA SILVA MARTINS on 16/03/22.
+//
+
+import UIKit
+import Foundation
+
+
+class XibComponent: UIView {
+    static let identifier = "XibComponent"
+
+    @IBOutlet var mainView: UIView!
+    @IBOutlet var textLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initSubviews()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initSubviews()
+    }
+
+    func initSubviews() {
+        
+        
+        guard let bundle = Bundle(for: type(of: self)) else {return}
+        let nib = UINib(nibName: XibComponent.identifier, bundle: bundle)
+
+        guard let view = nib.instantiate(withOwner: self, options: nil).first as?
+                            UIView else {fatalError("Unable to convert nib")}
+
+        view.frame = bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+        addSubview(view)
+
+    }
+    
+    func configureImageAndText(image : UIImage, text : String){
+        textLabel.text = text
+        imageView.image = image
+    }
+
+}
